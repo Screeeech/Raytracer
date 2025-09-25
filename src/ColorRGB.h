4afb7095fe9c667d0712957a880df127445527be
Lily Botha <lilybotha06@gmail.com>
2025-09-26 00:01:43 +0200

    cleanup

--- src/ColorRGB.h
+++ src/ColorRGB.h
@@ -1,129 +1,135 @@
 #pragma once
+#include <algorithm>
+
 #include "MathHelpers.h"
 
 namespace dae
 {
-	struct ColorRGB final
-	{
-		float r{};
-		float g{};
-		float b{};
+    struct ColorRGB final
+    {
+        float r{};
+        float g{};
+        float b{};
 
-		void MaxToOne()
-		{
-			const float maxValue = std::max(r, std::max(g, b));
-			if (maxValue > 1.f)
-				*this /= maxValue;
-		}
+        void MaxToOne()
+        {
+            const float maxValue = std::max({ r, g, b });
+            if(maxValue > 1.f)
+                *this /= maxValue;
+        }
 
-		static ColorRGB Lerp(const ColorRGB& c1, const ColorRGB& c2, float factor)
-		{
-			return { Lerpf(c1.r, c2.r, factor), Lerpf(c1.g, c2.g, factor), Lerpf(c1.b, c2.b, factor) };
-		}
+        static ColorRGB Lerp(const ColorRGB& c1, const ColorRGB& c2, float factor)
+        {
+            return { .r = Lerpf(c1.r, c2.r, factor),
+                     .g = Lerpf(c1.g, c2.g, factor),
+                     .b = Lerpf(c1.b, c2.b, factor) };
+        }
 
-		#pragma region ColorRGB (Member) Operators
-		const ColorRGB& operator+=(const ColorRGB& c)
-		{
-			r += c.r;
-			g += c.g;
-			b += c.b;
+#pragma region ColorRGB (Member) Operators
 
-			return *this;
-		}
+        const ColorRGB& operator+=(const ColorRGB& c)
+        {
+            r += c.r;
+            g += c.g;
+            b += c.b;
 
-		ColorRGB operator+(const ColorRGB& c) const
-		{
-			return { r + c.r, g + c.g, b + c.b };
-		}
+            return *this;
+        }
 
-		const ColorRGB& operator-=(const ColorRGB& c)
-		{
-			r -= c.r;
-			g -= c.g;
-			b -= c.b;
+        ColorRGB operator+(const ColorRGB& c) const
+        {
+            return { r + c.r, g + c.g, b + c.b };
+        }
 
-			return *this;
-		}
+        const ColorRGB& operator-=(const ColorRGB& c)
+        {
+            r -= c.r;
+            g -= c.g;
+            b -= c.b;
 
-		ColorRGB operator-(const ColorRGB& c) const
-		{
-			return { r - c.r, g - c.g, b - c.b };
-		}
+            return *this;
+        }
 
-		const ColorRGB& operator*=(const ColorRGB& c)
-		{
-			r *= c.r;
-			g *= c.g;
-			b *= c.b;
+        ColorRGB operator-(const ColorRGB& c) const
+        {
+            return { r - c.r, g - c.g, b - c.b };
+        }
 
-			return *this;
-		}
+        const ColorRGB& operator*=(const ColorRGB& c)
+        {
+            r *= c.r;
+            g *= c.g;
+            b *= c.b;
 
-		ColorRGB operator*(const ColorRGB& c) const
-		{
-			return { r * c.r, g * c.g, b * c.b };
-		}
+            return *this;
+        }
 
-		const ColorRGB& operator/=(const ColorRGB& c)
-		{
-			r /= c.r;
-			g /= c.g;
-			b /= c.b;
+        ColorRGB operator*(const ColorRGB& c) const
+        {
+            return { r * c.r, g * c.g, b * c.b };
+        }
 
-			return *this;
-		}
+        const ColorRGB& operator/=(const ColorRGB& c)
+        {
+            r /= c.r;
+            g /= c.g;
+            b /= c.b;
 
-		const ColorRGB operator/(const ColorRGB& c) const
-		{
-			return { r / c.r, g / c.g, b / c.b };
-		}
+            return *this;
+        }
 
-		const ColorRGB& operator*=(float s)
-		{
-			r *= s;
-			g *= s;
-			b *= s;
+        const ColorRGB operator/(const ColorRGB& c) const
+        {
+            return { r / c.r, g / c.g, b / c.b };
+        }
 
-			return *this;
-		}
+        const ColorRGB& operator*=(float s)
+        {
+            r *= s;
+            g *= s;
+            b *= s;
 
-		ColorRGB operator*(float s) const
-		{
-			return { r * s, g * s,b * s };
-		}
+            return *this;
+        }
 
-		const ColorRGB& operator/=(float s)
-		{
-			r /= s;
-			g /= s;
-			b /= s;
+        ColorRGB operator*(float s) const
+        {
+            return { r * s, g * s, b * s };
+        }
 
-			return *this;
-		}
+        const ColorRGB& operator/=(float s)
+        {
+            r /= s;
+            g /= s;
+            b /= s;
 
-		const ColorRGB operator/(float s) const
-		{
-			return { r / s, g / s, b / s };
-		}
-		#pragma endregion
-	};
+            return *this;
+        }
 
-	//ColorRGB (Global) Operators
-	inline ColorRGB operator*(float s, const ColorRGB& c)
-	{
-		return c * s;
-	}
+        const ColorRGB operator/(float s) const
+        {
+            return { r / s, g / s, b / s };
+        }
 
-	namespace colors
-	{
-		static ColorRGB Red{ 1,0,0 };
-		static ColorRGB Blue{ 0,0,1 };
-		static ColorRGB Green{ 0,1,0 };
-		static ColorRGB Yellow{ 1,1,0 };
-		static ColorRGB Cyan{ 0,1,1 };
-		static ColorRGB Magenta{ 1,0,1 };
-		static ColorRGB White{ 1,1,1 };
-		static ColorRGB Black{ 0,0,0 };
-		static ColorRGB Gray{ 0.5f,0.5f,0.5f };
-	}
-}
+#pragma endregion
+    };
+
+    // ColorRGB (Global) Operators
+    inline ColorRGB operator*(float s, const ColorRGB& c)
+    {
+        return c * s;
+    }
+
+    namespace colors
+    {
+        static ColorRGB Red{ 1, 0, 0 };
+        static ColorRGB Blue{ 0, 0, 1 };
+        static ColorRGB Green{ 0, 1, 0 };
+        static ColorRGB Yellow{ 1, 1, 0 };
+        static ColorRGB Cyan{ 0, 1, 1 };
+        static ColorRGB Magenta{ 1, 0, 1 };
+        static ColorRGB White{ 1, 1, 1 };
+        static ColorRGB Black{ 0, 0, 0 };
+        static ColorRGB Gray{ 0.5f, 0.5f, 0.5f };
+    }  // namespace colors
+}  // namespace dae
--- src/Utils.h
+++ src/Utils.h
@@ -1,149 +1,207 @@
 #pragma once
-#include "DataTypes.h"
-#include "Math.h"
+#include <cmath>
 #include <cstdint>
 #include <fstream>
+#include <iostream>
 
-namespace dae {
-namespace GeometryUtils {
+#include "DataTypes.h"
+#include "Math.h"
+#include "MathHelpers.h"
+#include "Vector3.h"
+
+namespace dae
+{
+namespace GeometryUtils
+{
 #pragma region Sphere HitTest
+
 // SPHERE HIT-TESTS
-inline bool HitTest_Sphere(const Sphere &sphere, const Ray &ray,
-                           HitRecord &hitRecord, bool ignoreHitRecord = false) {
-  // todo W1
-  throw std::runtime_error("Not Implemented Yet");
-  return false;
+inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray, HitRecord& hitRecord,
+                           bool ignoreHitRecord = false)
+{
+    // Quadratic equation
+    const float a{ Vector3::Dot(ray.direction, ray.direction) };
+    const float b{ 2 * Vector3::Dot(ray.direction, ray.origin - sphere.origin) };
+    const float c{ Vector3::Dot((ray.origin - sphere.origin), (ray.origin - sphere.origin)) -
+                   (sphere.radius * sphere.radius) };
+
+    const float discriminant{ (b * b) - (4 * a * c) };
+    if(discriminant <= 0)
+        return false;
+
+    float t{ (-b - sqrtf(discriminant)) / (2 * a) };
+    if(t < ray.min)
+        t = (-b + sqrtf(discriminant)) / (2 * a);
+
+    if(ignoreHitRecord)
+        return true;
+
+    const Vector3 hitPoint{ ray.origin + (t * ray.direction) };
+    hitRecord.origin = hitPoint;
+    hitRecord.didHit = true;
+    hitRecord.t = t;
+    hitRecord.materialIndex = sphere.materialIndex;
+    hitRecord.normal = (hitPoint - sphere.origin).Normalized();
+
+    return true;
 }
 
-inline bool HitTest_Sphere(const Sphere &sphere, const Ray &ray) {
-  HitRecord temp{};
-  return HitTest_Sphere(sphere, ray, temp, true);
+inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray)
+{
+    HitRecord temp{};
+    return HitTest_Sphere(sphere, ray, temp, true);
 }
+
 #pragma endregion
 #pragma region Plane HitTest
+
 // PLANE HIT-TESTS
-inline bool HitTest_Plane(const Plane &plane, const Ray &ray,
-                          HitRecord &hitRecord, bool ignoreHitRecord = false) {
-  // todo W1
-  throw std::runtime_error("Not Implemented Yet");
-  return false;
+inline bool HitTest_Plane(const Plane& plane, const Ray& ray, HitRecord& hitRecord,
+                          bool ignoreHitRecord = false)
+{
+    // todo W1
+    throw std::runtime_error("Not Implemented Yet");
+    return false;
 }
 
-inline bool HitTest_Plane(const Plane &plane, const Ray &ray) {
-  HitRecord temp{};
-  return HitTest_Plane(plane, ray, temp, true);
+inline bool HitTest_Plane(const Plane& plane, const Ray& ray)
+{
+    HitRecord temp{};
+    return HitTest_Plane(plane, ray, temp, true);
 }
+
 #pragma endregion
 #pragma region Triangle HitTest
+
 // TRIANGLE HIT-TESTS
-inline bool HitTest_Triangle(const Triangle &triangle, const Ray &ray,
-                             HitRecord &hitRecord,
-                             bool ignoreHitRecord = false) {
-  // todo W5
-  throw std::runtime_error("Not Implemented Yet");
-  return false;
+inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray, HitRecord& hitRecord,
+                             bool ignoreHitRecord = false)
+{
+    // todo W5
+    throw std::runtime_error("Not Implemented Yet");
+    return false;
 }
 
-inline bool HitTest_Triangle(const Triangle &triangle, const Ray &ray) {
-  HitRecord temp{};
-  return HitTest_Triangle(triangle, ray, temp, true);
+inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray)
+{
+    HitRecord temp{};
+    return HitTest_Triangle(triangle, ray, temp, true);
 }
+
 #pragma endregion
 #pragma region TriangeMesh HitTest
-inline bool HitTest_TriangleMesh(const TriangleMesh &mesh, const Ray &ray,
-                                 HitRecord &hitRecord,
-                                 bool ignoreHitRecord = false) {
-  // todo W5
-  throw std::runtime_error("Not Implemented Yet");
-  return false;
+
+inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray, HitRecord& hitRecord,
+                                 bool ignoreHitRecord = false)
+{
+    // todo W5
+    throw std::runtime_error("Not Implemented Yet");
+    return false;
 }
 
-inline bool HitTest_TriangleMesh(const TriangleMesh &mesh, const Ray &ray) {
-  HitRecord temp{};
-  return HitTest_TriangleMesh(mesh, ray, temp, true);
+inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray)
+{
+    HitRecord temp{};
+    return HitTest_TriangleMesh(mesh, ray, temp, true);
 }
+
 #pragma endregion
-} // namespace GeometryUtils
+}  // namespace GeometryUtils
 
-namespace LightUtils {
+namespace LightUtils
+{
 // Direction from target to light
-inline Vector3 GetDirectionToLight(const Light &light, const Vector3 origin) {
-  // todo W3
-  throw std::runtime_error("Not Implemented Yet");
-  return {};
+inline Vector3 GetDirectionToLight(const Light& light, const Vector3 origin)
+{
+    // todo W3
+    throw std::runtime_error("Not Implemented Yet");
+    return {};
 }
 
-inline ColorRGB GetRadiance(const Light &light, const Vector3 &target) {
-  // todo W3
-  throw std::runtime_error("Not Implemented Yet");
-  return {};
+inline ColorRGB GetRadiance(const Light& light, const Vector3& target)
+{
+    // todo W3
+    throw std::runtime_error("Not Implemented Yet");
+    return {};
 }
-} // namespace LightUtils
+}  // namespace LightUtils
 
-namespace Utils {
+namespace Utils
+{
 // Just parses vertices and indices
 #pragma warning(push)
-#pragma warning(disable : 4505) // Warning unreferenced local function
-static bool ParseOBJ(const std::string &filename,
-                     std::vector<Vector3> &positions,
-                     std::vector<Vector3> &normals, std::vector<int> &indices) {
-  std::ifstream file(filename);
-  if (!file)
-    return false;
+#pragma warning(disable : 4505)  // Warning unreferenced local function
 
-  std::string sCommand;
-  // start a while iteration ending when the end of file is reached (ios::eof)
-  while (!file.eof()) {
-    // read the first word of the string, use the >> operator
-    // (istream::operator>>)
-    file >> sCommand;
-    // use conditional statements to process the different commands
-    if (sCommand == "#") {
-      // Ignore Comment
-    } else if (sCommand == "v") {
-      // Vertex
-      float x, y, z;
-      file >> x >> y >> z;
-      positions.push_back({x, y, z});
-    } else if (sCommand == "f") {
-      float i0, i1, i2;
-      file >> i0 >> i1 >> i2;
+static bool ParseOBJ(const std::string& filename, std::vector<Vector3>& positions,
+                     std::vector<Vector3>& normals, std::vector<int>& indices)
+{
+    std::ifstream file(filename);
+    if(!file)
+        return false;
 
-      indices.push_back((int)i0 - 1);
-      indices.push_back((int)i1 - 1);
-      indices.push_back((int)i2 - 1);
-    }
-    // read till end of line and ignore all remaining chars
-    file.ignore(1000, '\n');
+    std::string sCommand;
+    // start a while iteration ending when the end of file is reached (ios::eof)
+    while(!file.eof())
+    {
+        // read the first word of the string, use the >> operator
+        // (istream::operator>>)
+        file >> sCommand;
+        // use conditional statements to process the different commands
+        if(sCommand == "#")
+        {
+            // Ignore Comment
+        }
+        else if(sCommand == "v")
+        {
+            // Vertex
+            float x, y, z;
+            file >> x >> y >> z;
+            positions.push_back({ x, y, z });
+        }
+        else if(sCommand == "f")
+        {
+            float i0, i1, i2;
+            file >> i0 >> i1 >> i2;
 
-    if (file.eof())
-      break;
-  }
+            indices.push_back((int)i0 - 1);
+            indices.push_back((int)i1 - 1);
+            indices.push_back((int)i2 - 1);
+        }
+        // read till end of line and ignore all remaining chars
+        file.ignore(1000, '\n');
 
-  // Precompute normals
-  for (uint64_t index = 0; index < indices.size(); index += 3) {
-    uint32_t i0 = indices[index];
-    uint32_t i1 = indices[index + 1];
-    uint32_t i2 = indices[index + 2];
-
-    Vector3 edgeV0V1 = positions[i1] - positions[i0];
-    Vector3 edgeV0V2 = positions[i2] - positions[i0];
-    Vector3 normal = Vector3::Cross(edgeV0V1, edgeV0V2);
-
-    if (std::isnan(normal.x)) {
-      int k = 0;
+        if(file.eof())
+            break;
     }
 
-    normal.Normalize();
-    if (std::isnan(normal.x)) {
-      int k = 0;
+    // Precompute normals
+    for(uint64_t index = 0; index < indices.size(); index += 3)
+    {
+        uint32_t i0 = indices[index];
+        uint32_t i1 = indices[index + 1];
+        uint32_t i2 = indices[index + 2];
+
+        Vector3 edgeV0V1 = positions[i1] - positions[i0];
+        Vector3 edgeV0V2 = positions[i2] - positions[i0];
+        Vector3 normal = Vector3::Cross(edgeV0V1, edgeV0V2);
+
+        if(std::isnan(normal.x))
+        {
+            int k = 0;
+        }
+
+        normal.Normalize();
+        if(std::isnan(normal.x))
+        {
+            int k = 0;
+        }
+
+        normals.push_back(normal);
     }
 
-    normals.push_back(normal);
-  }
-
-  return true;
+    return true;
 }
+
 #pragma warning(pop)
-} // namespace Utils
-} // namespace dae
+}  // namespace Utils
+}  // namespace dae
