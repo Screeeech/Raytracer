5bb0952507c87b090a7442b8eb607444dee73997
Lily Botha <lilybotha06@gmail.com>
2025-10-05 13:32:57 +0200

    add nodiscard to Matrix.h

--- src/Matrix.h
+++ src/Matrix.h
@@ -14,21 +14,21 @@ struct Matrix final
     Matrix(const Vector3& xAxis, const Vector3& yAxis, const Vector3& zAxis, const Vector3& t);
     Matrix(const Vector4& xAxis, const Vector4& yAxis, const Vector4& zAxis, const Vector4& t);
 
-    Vector3 TransformVector(const Vector3& v) const;
-    Vector3 TransformVector(float x, float y, float z) const;
-    Vector3 TransformPoint(const Vector3& p) const;
-    Vector3 TransformPoint(float x, float y, float z) const;
+    [[nodiscard]] Vector3 TransformVector(const Vector3& v) const;
+    [[nodiscard]] Vector3 TransformVector(float x, float y, float z) const;
+    [[nodiscard]] Vector3 TransformPoint(const Vector3& p) const;
+    [[nodiscard]] Vector3 TransformPoint(float x, float y, float z) const;
 
-    Vector4 TransformPoint(const Vector4& p) const;
-    Vector4 TransformPoint(float x, float y, float z, float w) const;
+    [[nodiscard]] Vector4 TransformPoint(const Vector4& p) const;
+    [[nodiscard]] Vector4 TransformPoint(float x, float y, float z, float w) const;
 
     const Matrix& Transpose();
     const Matrix& Inverse();
 
-    Vector3 GetAxisX() const;
-    Vector3 GetAxisY() const;
-    Vector3 GetAxisZ() const;
-    Vector3 GetTranslation() const;
+    [[nodiscard]] Vector3 GetAxisX() const;
+    [[nodiscard]] Vector3 GetAxisY() const;
+    [[nodiscard]] Vector3 GetAxisZ() const;
+    [[nodiscard]] Vector3 GetTranslation() const;
 
     static Matrix CreateIdentity();
     static Matrix CreateTranslation(float x, float y, float z);
