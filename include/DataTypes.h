ff979fde0af87c3f46d71094c16a1dcd5752e012
Lily Botha <lilybotha06@gmail.com>
2025-10-21 09:16:42 +0200

    Updates project structure and implemented CalculateNormals and UpdateTransform

---
+++ include/DataTypes.h
@@ -0,0 +1,222 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <stdexcept>
+#include <vector>
+
+#include "Math.h"
+#include "Vector3.h"
+
+namespace dae
+{
+#pragma region GEOMETRY
+
+struct Sphere final
+{
+    Vector3 origin;
+    float radius{};
+
+    unsigned char materialIndex{ 0 };
+};
+
+struct Plane final
+{
+    Vector3 origin;
+    Vector3 normal;
+
+    unsigned char materialIndex{ 0 };
+};
+
+enum class TriangleCullMode : uint8_t
+{
+    FrontFaceCulling,
+    BackFaceCulling,
+    NoCulling
+};
+
+struct Triangle final
+{
+    Triangle() = default;
+
+    Triangle(const Vector3& _v0, const Vector3& _v1, const Vector3& _v2, const Vector3& _normal)
+        : v0{ _v0 }
+        , v1{ _v1 }
+        , v2{ _v2 }
+        , normal{ _normal.Normalized() }
+    {
+    }
+
+    Triangle(const Vector3& _v0, const Vector3& _v1, const Vector3& _v2)
+        : v0{ _v0 }
+        , v1{ _v1 }
+        , v2{ _v2 }
+    {
+        const Vector3 edgeV0V1 = v1 - v0;
+        const Vector3 edgeV0V2 = v2 - v0;
+        normal = Vector3::Cross(edgeV0V1, edgeV0V2).Normalized();
+    }
+
+    Vector3 v0;
+    Vector3 v1;
+    Vector3 v2;
+
+    Vector3 normal;
+
+    TriangleCullMode cullMode{};
+    unsigned char materialIndex{};
+};
+
+struct TriangleMesh final
+{
+    TriangleMesh() = default;
+
+    TriangleMesh(const std::vector<Vector3>& _positions, const std::vector<int>& _indices, TriangleCullMode _cullMode)
+        : positions(_positions)
+        , indices(_indices)
+        , cullMode(_cullMode)
+    {
+        // Calculate Normals
+        CalculateNormals();
+
+        // Update Transforms
+        UpdateTransforms();
+    }
+
+    TriangleMesh(const std::vector<Vector3>& _positions, const std::vector<int>& _indices, const std::vector<Vector3>& _normals,
+                 TriangleCullMode _cullMode)
+        : positions(_positions)
+        , normals(_normals)
+        , indices(_indices)
+        , cullMode(_cullMode)
+    {
+        UpdateTransforms();
+    }
+
+    std::vector<Vector3> positions;
+    std::vector<Vector3> normals;
+    std::vector<int> indices;
+    unsigned char materialIndex{};
+
+    TriangleCullMode cullMode{ TriangleCullMode::BackFaceCulling };
+
+    Matrix rotationTransform;
+    Matrix translationTransform;
+    Matrix scaleTransform;
+
+    std::vector<Vector3> transformedPositions;
+    std::vector<Vector3> transformedNormals;
+
+    void Translate(const Vector3& translation)
+    {
+        translationTransform = Matrix::CreateTranslation(translation);
+    }
+
+    void RotateY(float yaw)
+    {
+        rotationTransform = Matrix::CreateRotationY(yaw);
+    }
+
+    void Scale(const Vector3& scale)
+    {
+        scaleTransform = Matrix::CreateScale(scale);
+    }
+
+    void AppendTriangle(const Triangle& triangle, bool ignoreTransformUpdate = false)
+    {
+        int startIndex = static_cast<int>(positions.size());
+
+        positions.push_back(triangle.v0);
+        positions.push_back(triangle.v1);
+        positions.push_back(triangle.v2);
+
+        indices.push_back(startIndex);
+        indices.push_back(++startIndex);
+        indices.push_back(++startIndex);
+
+        normals.push_back(triangle.normal);
+
+        // Not ideal, but making sure all vertices are updated
+        if(!ignoreTransformUpdate)
+            UpdateTransforms();
+    }
+
+    void CalculateNormals()
+    {
+        size_t i{};
+        std::array<int, 3> triVertexIndices{};
+        for(const int index : indices)
+        {
+            triVertexIndices.at(i) = index;
+            ++i;
+            if(i >= 2)
+            {
+                const Vector3 vectorA{ positions[triVertexIndices[0]], positions[triVertexIndices[1]] };
+                const Vector3 vectorB{ positions[triVertexIndices[1]], positions[triVertexIndices[2]] };
+                normals.push_back((Vector3::Cross(vectorA, vectorB)).Normalized());
+                i = 0;
+            }
+        }
+    }
+
+    void UpdateTransforms()
+    {
+        for(const Vector3& position : positions)
+        {
+            auto transformedPosition = translationTransform.TransformPoint(position);
+            transformedPosition = rotationTransform.TransformPoint(transformedPosition);
+            transformedPosition = scaleTransform.TransformPoint(transformedPosition);
+            transformedPositions.push_back(transformedPosition);
+        }
+
+        for(const Vector3& normal : normals)
+        {
+            auto transformedNormal = translationTransform.TransformPoint(normal);
+            transformedNormal = rotationTransform.TransformPoint(transformedNormal);
+            transformedNormal = scaleTransform.TransformPoint(transformedNormal);
+            transformedNormals.push_back(transformedNormal);
+        }
+    }
+};
+
+#pragma endregion
+#pragma region LIGHT
+enum class LightType : uint8_t
+{
+    Point,
+    Directional
+};
+
+struct Light final
+{
+    Vector3 origin;
+    Vector3 direction;
+    ColorRGB color{};
+    float intensity{};
+
+    LightType type{};
+};
+
+#pragma endregion
+#pragma region MISC
+
+struct Ray final
+{
+    Vector3 origin;
+    Vector3 direction;
+
+    float min{ 0.0001f };
+    float max{ FLT_MAX };
+};
+
+struct HitRecord final
+{
+    Vector3 origin{};
+    Vector3 normal{};
+    float t = FLT_MAX;
+
+    bool didHit{ false };
+    unsigned char materialIndex{ 0 };
+};
+
+#pragma endregion
+}  // namespace dae
